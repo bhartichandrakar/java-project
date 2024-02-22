3a95878c1a637af1ed673a1f8a49514eba12eb53
VarunKashyap1810 <VarunKashyap1810@gmail.com>
2024-02-22 09:03:53 +0530

    2202

---
+++ LeetFull/LongestCommonPrefix.java
@@ -0,0 +1,71 @@
+package LeetFull;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        String result = longestCommonPrefix(new String[]{"dog","racecar","car"});
+        System.out.println(result);
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        //Checks if the input array strs is null or empty. If so, returns an empty string as there's no common prefix.
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        //Sorts the array of strings using Arrays.sort(strs). This ensures that strings with a common prefix will be placed consecutively.
+        Arrays.sort(strs);
+        //Initializes index to 0 to track character positions.
+        int index = 0;
+        //Gets the first and last strings from the sorted array (first = strs[0]
+        String first = strs[0];
+        //last = strs[strs.length - 1])
+        String last = strs[strs.length - 1];
+        //Enters a while loop that continues as long as index is within the bounds of the first string's length.
+        while(index < first.length()){
+            //Compares the characters at the current index of both first and last strings.
+            if(first.charAt(index)==last.charAt(index)){
+                //If the characters match, increments index to move to the next character.
+                index++;
+            }else{
+                //If the characters don't match, breaks out of the loop as a mismatch indicates the end of the common prefix.
+                break;
+            }
+        }
+        //If index is still 0 after the loop, it means no common prefix was found, so returns an empty string.
+        //Otherwise, returns a substring of first from the beginning up to (but not including) index, representing the longest common prefix.
+        return index==0?"":first.substring(0, index);
+    }
+    
+}
+
+//Time Complexity: O(S log S), where S is the sum of the lengths of all strings. Sorting takes O(S log S) time, and the remaining comparisons take O(S) time.
+//Space Complexity: O(1), as the algorithm uses constant extra space for variables regardless of the input size.
+//Sorting Trade-off: Sorting the strings adds a log S factor to the time complexity, but it simplifies the comparison process.
+
+
+
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+ 
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+ 
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters.
---
+++ LeetFull/ValidParentheses.java
@@ -0,0 +1,110 @@
+package LeetFull;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        boolean result = isValid("()");
+        System.out.println(result);
+    }
+
+    public static boolean isValid(String s) {
+        //Initialize an empty stack to store opening parentheses temporarily.
+        Stack<Character> stack = new Stack<Character>();
+        //Loop through each character in the input string.
+        for(Character c : s.toCharArray()){
+            //If the current character is an opening parenthesis ((, {, [), push it onto the stack.
+            if (c=='(' || c=='{' || c=='[') {
+                stack.push(c);
+            }
+            //If the current character is a closing parenthesis :
+            //Check if the stack is empty (mismatched pair if true).
+            //Pop the top element from the stack.
+            //If the popped element doesn't match the expected opening pair for the current closing parenthesis, return false (invalid sequence).
+            else if (c==')' && (stack.isEmpty() || stack.pop() != '(')) {
+                    return false;
+            }else if (c=='}' && (stack.isEmpty() || stack.pop() != '{')) {
+                return false;
+            }else if (c==']' && (stack.isEmpty() || stack.pop() != '[')) {
+                return false;
+            }
+        }
+        //After iterating through all characters, if the stack is empty, all parentheses are paired correctly, return true; otherwise, unpaired parentheses exist, return false.
+        return stack.isEmpty();
+
+
+    //Time Complexity: O(n), where n is the length of the input string. Each character is processed only once (push or pop on stack).
+    //Space Complexity: O(n) in the worst case. The stack might store all opening parentheses if they're at the beginning of the string and closing ones are later.
+
+    //Alternative Approach:
+    //Another common approach is using a HashMap to map opening parentheses to their closing counterparts. Time complexity remains O(n), but space complexity might be reduced to O(1) if only limited types of parentheses are considered.
+    }
+
+
+    // public static boolean isValid(String s) {
+    //     Map<Character, Character> mappings = new HashMap<>();
+    //     mappings.put(')', '(');
+    //     mappings.put('}', '{');
+    //     mappings.put(']', '[');
+
+    //     Stack<Character> stack = new Stack<>();
+    //     for (char c : s.toCharArray()) {
+    //         if (mappings.containsKey(c)) {
+    //             char topElement = stack.isEmpty() ? '#' : stack.pop();
+    //             if (topElement != mappings.get(c)) {
+    //                 return false;
+    //             }
+    //         } else {
+    //             stack.push(c);
+    //         }
+    //     }
+    //     return stack.isEmpty();
+    // }
+
+
+    // public static boolean isValid(String s) {
+    //     Stack<Character> stack = new Stack<>();
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(' || c == '{' || c == '[') {
+    //             stack.push(c);
+    //         } else {
+    //             if (stack.isEmpty()) return false;
+    //             char open = stack.pop();
+    //             if (c == ')' && open != '(') return false;
+    //             if (c == '}' && open != '{') return false;
+    //             if (c == ']' && open != '[') return false;
+    //         }
+    //     }
+    //     return stack.isEmpty();
+    // }
+}
+
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
