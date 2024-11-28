98b649375d03ff8fc7f750bcb21c72db395dd9b7
VarunKashyap1810 <VarunKashyap1810@gmail.com>
2024-11-28 13:38:32 +0530

    L215KthLargestElementArray

--- PDF/L215KthLargestElementArray.java
+++ PDF/L215KthLargestElementArray.java
@@ -5,11 +5,9 @@ import java.util.Arrays;
 public class L215KthLargestElementArray {
 
     public static void main(String[] args) {
-        int[] nums = {3,2,3,1,2,4,5,5,6};
-
-        int result = findKthLargest(nums, 4);
-        System.out.println(result);
-        
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+        System.out.println("The " + k + "-th largest element is: " + findKthLargest(nums, nums.length - k + 1));        
     }
 
     public static int findKthLargest(int[] nums, int k) {
@@ -19,20 +17,49 @@ public class L215KthLargestElementArray {
         /* Arrays.sort(nums);
         int result = nums[nums.length - k]; */
 
-        int largest = nums[0];
-        int secondLargest = 0;
-
-        for(int i = 1; i < nums.length - 1; i++ ){
-            if (nums[i] > largest) {
-                secondLargest = largest;
-                largest = nums[i];
-            }else{
-                if (secondLargest < nums[i]) {
-                    secondLargest = nums[i];
-                }
-            }
-
+        int result = quickSelect(nums,k);
+        return result;
+    }
+        
+    public static int quickSelect(int[] nums, int k) {
+        return quickSelectHelper(nums, 0 , nums.length - 1, k - 1);
+    }
+        
+    private static int quickSelectHelper(int[] nums, int low, int high, int k) {
+        if (low == high) {
+            return nums[low];
         }
-        return secondLargest;
+        int pivotIndex =  partition(nums, low, high);
+        if (pivotIndex == k) { // Found the k-th smallest element
+            return nums[pivotIndex];
+        } else if (k < pivotIndex) { // Search in the left part
+            return quickSelectHelper(nums, low, pivotIndex - 1, k);
+        } else { // Search in the right part
+            return quickSelectHelper(nums, pivotIndex + 1, high, k);
+        }
+
+    }
+        
+    private static int partition(int[] nums, int low, int high) {
+        if (low == high) {
+            return nums[low];
+        }
+
+        int pivot = nums[high];
+        int i = low;
+
+        for(int j = low; j < high; j++){
+            if(nums[j] < pivot){
+                swap(nums, i , j);
+                i++;
+            }
+        }
+        swap(nums, i, high);
+        return i;
+    }
+    private static void swap(int[] nums, int i, int j) {
+       int temp = nums[i];
+       nums[i] = nums[j];
+       nums[j] = temp;
     }
 }
